79bc1540a9a9826ccd6fdb3e3650d87681fe8bf2
puji1104klu <2300032329@kluniversity.in>
2026-03-12 15:20:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,7 +1,39 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) 
     {
-        //
+        // //Brute Force
+        // //O(n2)
+        
+        // for(int i=0; i<nums.length; i++)
+        // {
+        //     for(int j=i+1; j<nums.length; j++)
+        //     {
+        //         if(nums[i]==nums[j])
+        //         {
+        //             return true;
+        //         }
+        //     }
+        
+        // }
+        // return false;
+
+
+        // //Better solution
+        // //O(n)
+
+        // Arrays.sort(nums);
+        // for(int i=1; i<nums.length; i++)
+        // {
+        //     if(nums[i]==nums[i-1])
+        //     {
+        //         return true;
+        //     }
+        // }
+        // return false;  
+
+
+
+        //Optimal - O(n)
         HashSet<Integer> hs= new HashSet<>();
         for(int i=0; i<nums.length; i++)
         {
